d234ad85e3365dc6b535c45bcd112e4c7b001786
andrei <roanflo05@gmail.com>
2021-10-17 12:58:46 +0300

    bot neinteligent

--- js/script.js
+++ js/script.js
@@ -19,22 +19,16 @@ var clickHandler = (event) => {
         return
     }
 
-    moves++;
+     if (makeMove(target)) {
+         return
+     }
 
-    target.addClass(player);
-    target.html(player);
-
-    winner = checkWinner()
-
-    if (winner) {
-        winnerDiv.html("Winner: " + winner)
-        resetDiv.show();
-    }
-    
-     player = player == "X" ? "O" : "X";
+     botMove();
 };
 
 var resetGame = () => {
+    player = "X"
+
     resetDiv.hide();
 
     winnerDiv.html("");
@@ -57,15 +51,45 @@ var checkWinner = () => {
         }
     );
 
-    if (values[0] === values[4] && values[4] === values[8]) return values[0];
-    if (values[2] === values[4] && values[4] === values[6]) return values[2];
+    if (values[0] === values[4] && values[4] === values[8] && values[0]) return values[0];
+    if (values[2] === values[4] && values[4] === values[6] && values[2]) return values[2];
 
     for (let i = 0; i < 3; i++) {        
-        if (values[i] === values[i + 3] && values[i + 3] === values[i + 6]) return values[i];
-        if (values[i * 3] === values[i * 3 + 1] && values[i * 3 + 1] === values[i * 3 + 2]) return values[i * 3];
+        if (values[i] === values[i + 3] && values[i + 3] === values[i + 6] && values[i]) return values[i];
+        if (values[i * 3] === values[i * 3 + 1] && values[i * 3 + 1] === values[i * 3 + 2] && values[i * 3]) return values[i * 3];
     }
 
     if(moves === 9) return "DRAW";
+}
+
+var botMove = () => {
+    var move = Math.floor(Math.random() * 9);
+    var botButton = $(buttons[move]);
+
+    while(botButton.html()) {
+        move = Math.floor(Math.random() * 9);
+        botButton = $(buttons[move]);
+    }
+
+    if (makeMove(botButton)) {
+        return;
+    }
+}
+
+var makeMove = (button) => {
+    moves++;
+    button.addClass(player);
+    button.html(player);
+
+    winner = checkWinner();
+
+    if (winner) {
+        winnerDiv.html("Winner: " + winner)
+        resetDiv.show();
+        return true;
+    }
+    
+     player = player == "X" ? "O" : "X";
 }
 
 resetDiv.click(resetGame);
